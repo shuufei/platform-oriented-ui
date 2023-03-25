cc5063337cc5126e308e2e6aaf4aa7e4ff3c5531
shuufei <digital.shuufei@gmail.com>
2023-03-25 19:44:36 +0900

    fix: material3の追加

--- material-design-3-sandbox/app/src/main/java/com/example/materialdesign3sandbox/MainActivity.kt
+++ material-design-3-sandbox/app/src/main/java/com/example/materialdesign3sandbox/MainActivity.kt
@@ -3,26 +3,31 @@ package com.example.materialdesign3sandbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.materialdesign3sandbox.ui.theme.AppThemeM3
 import com.example.materialdesign3sandbox.ui.theme.MaterialDesign3SandboxTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialDesign3SandboxTheme {
-                // A surface container using the 'background' color from the theme
+//            MaterialDesign3SandboxTheme {
+            AppThemeM3 {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.surface
                 ) {
-                    Greeting("Android")
+                    Column() {
+                        Greeting("Android")
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Filled Button")
+                        }
+                    }
                 }
             }
         }
